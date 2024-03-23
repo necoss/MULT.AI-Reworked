b2b085ed604f7a16a0b8f6a2b081f6ad445b89ae
trexi <vartypeez@gmail.com>
2024-03-24 00:01:35 +0300

    ver 1.0

---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+// const soundbutton = document.querySelector('.navigation-right__music-button')
+// const audio = document.querySelector('.navigation_music')
+// const navigationMusicIcon = document.querySelector('.navigation_music-icon')
+
+// // navigationMusicIcon.playerInstance.play()
+
+// audio.volume = 0.1
+
+// soundbutton.addEventListener('click', e => {
+//   soundbutton.classList.toggle('paused')
+//   audio.paused ? audio.play() : audio.pause()
+// })
+
+// window.onfocus = function() {
+//   soundbutton.classList.contains('paused') ? audio.pause() : audio.play()
+// }
+
+// window.onblur = function() {
+//   audio.pause()
+// }
+
